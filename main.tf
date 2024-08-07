9aef193d6c92349f8d80378eee47ac358c156c6a
knjoroge <kenneth.njoroge@zencargo.com>
2024-08-07 15:04:06 -0400

    Remove subnet_id

--- main.tf
+++ main.tf
@@ -15,5 +15,4 @@ data "terraform_remote_state" "network" {
 resource "aws_instance" "app" {
   ami           = "ami-0ae8f15ae66fe8cda"
   instance_type = "t2.micro"
-  subnet_id     = data.terraform_remote_state.network.outputs.subnet_id
 }
