23553dca47f5762d8c81056f0c07bab048702d8e
knjoroge <kenneth.njoroge@zencargo.com>
2024-08-06 20:46:29 -0400

    revert changes

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "terraform_remote_state" "application" {
+data "terraform_remote_state" "network" {
   backend = "remote"
   config = {
     organization = "knjoroge-org"
     workspaces = {
-      name = "aws-1-application"
+      name = "aws-0-network"
     }
   }
 }
@@ -15,5 +15,5 @@ data "terraform_remote_state" "application" {
 resource "aws_instance" "app" {
   ami           = "ami-0ae8f15ae66fe8cda"
   instance_type = "t2.micro"
-  subnet_id     = data.terraform_remote_state.application.outputs.subnet_id
+  subnet_id     = data.terraform_remote_state.network.outputs.subnet_id
 }
